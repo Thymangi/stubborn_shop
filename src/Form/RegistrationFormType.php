233540be1f54e391ddcc62200d00c684a6bb363d
Thymangi <154999272+Thymangi@users.noreply.github.com>
2025-09-02 15:06:15 +0200

    version au 02-09-2025, site stabel avec tests

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,12 +5,17 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
+// Types de champs
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+// Contraintes
 use Symfony\Component\Validator\Constraints as Assert;
 
 class RegistrationFormType extends AbstractType
@@ -20,41 +25,55 @@ class RegistrationFormType extends AbstractType
         $builder
             // correspond à User::$name
             ->add('name', TextType::class, [
-                'required' => false,
-                'label' => 'Nom / Pseudo',
+                'required'   => false,
+                'label'      => 'Nom / Pseudo',
                 'empty_data' => '',
             ])
+
             ->add('email', EmailType::class, [
-                'label' => 'E-mail',
+                'label'       => 'E-mail',
                 'constraints' => [
                     new Assert\NotBlank(['message' => 'Merci de renseigner un e-mail.']),
                     new Assert\Email(['message' => 'E-mail invalide.']),
                     new Assert\Length(['max' => 180]),
                 ],
             ])
+
             // correspond à User::$deliveryAddress
             ->add('deliveryAddress', TextType::class, [
-                'required' => false,
-                'label' => 'Adresse de livraison',
+                'required'   => false,
+                'label'      => 'Adresse de livraison',
                 'empty_data' => '',
             ])
-            // mot de passe non mappé : on le hash dans le contrôleur
-            ->add('plainPassword', PasswordType::class, [
-                'label' => 'Mot de passe',
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new Assert\NotBlank(['message' => 'Merci de saisir un mot de passe.']),
-                    new Assert\Length([
-                        'min' => 8,
-                        'minMessage' => 'Au moins {{ limit }} caractères.',
-                        'max' => 4096,
-                    ]),
+
+            // Mot de passe en 2 champs (confirmation), non mappé sur l’entité.
+            ->add('plainPassword', RepeatedType::class, [
+                'type'            => PasswordType::class,
+                'mapped'          => false,
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+                // Options du 1er champ
+                'first_options'   => [
+                    'label'       => 'Mot de passe',
+                    'attr'        => ['autocomplete' => 'new-password'],
+                    'constraints' => [
+                        new Assert\NotBlank(['message' => 'Merci de saisir un mot de passe.']),
+                        new Assert\Length([
+                            'min'        => 8,
+                            'minMessage' => 'Au moins {{ limit }} caractères.',
+                            'max'        => 4096,
+                        ]),
+                    ],
+                ],
+                // Options du 2e champ (confirmation)
+                'second_options'  => [
+                    'label' => 'Confirmer le mot de passe',
+                    'attr'  => ['autocomplete' => 'new-password'],
                 ],
             ])
+
             ->add('agreeTerms', CheckboxType::class, [
-                'label' => "J'accepte les CGU",
-                'mapped' => false,
+                'label'       => "J'accepte les CGU",
+                'mapped'      => false,
                 'constraints' => [
                     new Assert\IsTrue(['message' => 'Vous devez accepter les conditions.']),
                 ],
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,16 @@
 <?php
+// tests/bootstrap.php
 
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
+// Charge les variables d'environnement (.env, .env.test, etc.)
 if (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
 
+// Configure le mode debug
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
